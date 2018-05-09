649d2c6a5403f1421102aa82ff754f2028a78703
Matt <matthew.watts.mw@gmail.com>
2018-05-09 18:22:36 -0500

    collision detection perfected.

--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@ import rules from './rules.js';
 import predicates from './predicates';
 import { evaluate } from 'regent';
 
-const {PLAYER_IS_BLOCKED} = rules;
-const {overlaps} = predicates;
+const SPEED = 5;
+const { PLAYER_IS_BLOCKED } = rules;
+const { overlaps } = predicates;
 
 let playerState = {};
 let obstacles = [];
 
 const sketch = p5 => {
     window.p5 = p5;
-    p5.setup = () => {        
+    p5.setup = () => {
         p5.createCanvas(600, 400);
         p5.rectMode(p5.CENTER);
 
@@ -29,67 +30,50 @@ const sketch = p5 => {
             width: 20,
             height: 20
         });
+
+        obstacles.push({
+            xpos: p5.width / 4,
+            ypos: p5.height / 2,
+            width: 20,
+            height: 20
+        });
     }
 
     p5.draw = () => {
         p5.background(0);
         updateGuy(p5);
         drawGuy(p5);
-        drawObstacle(p5);
+        drawObstacles(p5);
     }
 }
-
-const P5 = new p5(sketch);  
 
 const updateGuy = (p5) => {
     const PIB = PLAYER_IS_BLOCKED;
+    let { xpos, ypos } = playerState;
 
-    if (p5.keyIsDown(p5.UP_ARROW)
-        && allObjectsAreNotInWay(obstacles, (obstacleState) => {
-            evaluate(PIB, { "playerState": {...playerState, ypos:playerState.ypos-5}, obstacleState },{ overlaps })
-        })) 
-        { 
-            console.log("allObjectsAreNotInWay:"+allObjectsAreNotInWay(obstacles, (obstacleState) => {
-                evaluate(PIB, { "playerState": {...playerState, ypos:playerState.ypos-5}, obstacleState },{ overlaps })
-            }))
-        
-            console.log("Evaluate Player Is Blocked:"+evaluate(PIB, { "playerState": {...playerState, ypos:playerState.ypos-5}, 'obstacleState': obstacles[0] },{ overlaps }));
-        
-            playerState.ypos -= 5;
-        }
+    if (p5.keyIsDown(p5.UP_ARROW) && allFail(obstacles, obstacleState => evaluate(
+        PIB, { "playerState": { ...playerState, ypos: ypos - SPEED }, obstacleState }, { overlaps }))
+    ) playerState.ypos -= SPEED;
 
-        // THIS WORKS====>
-        // !evaluate(PIB, { "playerState": {...playerState, ypos:playerState.ypos-5}, 'obstacleState': obstacles[0] },{ overlaps }))
-        //     playerState.ypos -= 5;
+    if (p5.keyIsDown(p5.DOWN_ARROW) && allFail(obstacles, obstacleState => evaluate(
+        PIB, { "playerState": { ...playerState, ypos: ypos + SPEED }, obstacleState }, { overlaps }))
+    ) playerState.ypos += SPEED;
 
-        // anyPass(obstacles, (obstacleState)=>{
-        // }))
-    
-    if (p5.keyIsDown(p5.DOWN_ARROW)
-        && allObjectsAreNotInWay(obstacles, (obstacleState)=>{
-           evaluate(PIB, { "playerState": {...playerState, ypos:playerState.ypos+5},  obstacleState },{ overlaps })
-        }))
-        playerState.ypos += 5;
+    if (p5.keyIsDown(p5.LEFT_ARROW) && allFail(obstacles, obstacleState => evaluate(
+        PIB, { "playerState": { ...playerState, xpos: xpos - SPEED }, obstacleState }, { overlaps }))
+    ) playerState.xpos -= SPEED;
 
-    if (p5.keyIsDown(p5.LEFT_ARROW)
-        && allObjectsAreNotInWay(obstacles, (obstacleState)=>{
-           evaluate(PIB, { "playerState": {...playerState, xpos:playerState.xpos-5},  obstacleState },{ overlaps })
-        }))
-        playerState.xpos -= 5;
-
-    if (p5.keyIsDown(p5.RIGHT_ARROW)
-        && allObjectsAreNotInWay(obstacles, (obstacleState)=>{
-           evaluate(PIB, { "playerState": {...playerState, xpos:playerState.xpos+5},  obstacleState },{ overlaps })
-        }))
-        playerState.xpos += 5;
+    if (p5.keyIsDown(p5.RIGHT_ARROW) && allFail(obstacles, obstacleState => evaluate(
+        PIB, { "playerState": { ...playerState, xpos: xpos + SPEED }, obstacleState }, { overlaps })
+    )) playerState.xpos += SPEED;
 }
 
-const drawGuy = p5 => p5.rect(playerState.xpos, playerState.ypos, 20, 20); 
-const drawObstacle = p5 => p5.rect(obstacles[0].xpos, obstacles[0].ypos, 20, 20);
+const drawGuy = p5 => p5.rect(playerState.xpos, playerState.ypos, 20, 20);
+const drawObstacles = p5 => obstacles.map(ob => p5.rect(ob.xpos, ob.ypos, 20, 20));
 
-const allObjectsAreNotInWay = (arr, fxn) => {
-    for(let a of arr){
-        if(fxn(a)) return false;   
-    }
+const allFail = (arr, fxn) => {
+    for (let a of arr) if (fxn(a)) return false;
     return true;
 }
+
+const P5 = new p5(sketch);
