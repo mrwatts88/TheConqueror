6efd5b5d01975db4e4dd5640414dc533b1496a6b
Matt Watts <matthew.watts.mw@gmail.com>
2018-05-14 21:30:16 -0500

    change potion to add 25 health from 50

--- src/performClickAction.js
+++ src/performClickAction.js
@@ -1,7 +1,7 @@
 import { BS } from './constants';
 
 // Perform the correct action based on what was clicked
-// Currently, only handles removing inventory
+// Currently, only handles removing inventory (3 potions will add 25/100 health)
 export const performClickAction = (p5, xpos, ypos, player) => {
 
     let baseX = p5.width - 150;
@@ -11,16 +11,13 @@ export const performClickAction = (p5, xpos, ypos, player) => {
         && xpos <= baseX + 4 * (BS + 5)
         && ypos <= baseY + 4 * (BS + 5)) {
         let item = player.inventory.splice(getClickedInventoryIndex(xpos, ypos, baseX, baseY), 1);
-        if (item[0].type === 'ii' || item[0] === 'ij' || item[0] === 'ik') {
-            player.health += 50;
-            if (player.health > player.maxHealth) player.health = player.maxHealth;
-        }
+        if (item[0].type === 'ii' || item[0].type === 'ij' || item[0].type === 'ik')
+            player.health = Math.min(player.health + 25, 100);
     }
 }
 
 const getClickedInventoryIndex = (xpos, ypos, baseX, baseY) => {
     let xIndex = Math.floor((xpos - baseX) / (BS + 5));
     let yIndex = Math.floor((ypos - baseY) / (BS + 5));
-
     return yIndex * 4 + xIndex;
 }
