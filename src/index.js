655b543c98c99a9a68f41fe4afb09dbd22f93a6a
Matt Watts <matthew.watts.mw@gmail.com>
2018-05-15 21:36:50 -0500

    smooth scroll

--- src/index.js
+++ src/index.js
@@ -17,7 +17,9 @@ let player = [{}];
 let enemies = [];
 let map = [];
 let frame = 0;
-let images, enemyImages, mapImage, itemImage, startCorner;
+let images, enemyImages, mapImage, itemImage, startCorner, next;
+let state = 'PLAY';
+let glideCount = 0;
 
 const sketch = p5 => {
     p5.preload = () => {
@@ -36,24 +38,59 @@ const sketch = p5 => {
             performClickAction(p5, p5.mouseX, p5.mouseY, player[0]);
         })
     }
-
+    let superMove;
 
     p5.draw = () => {
         p5.background(255);
-        startCorner = shiftView(player, p5, startCorner, enemies, mapImage, map);
-        drawEntities(p5, map, startCorner, enemies, itemImage);
-        drawGuy(p5, player[0], images);
-        drawEnemies(p5, enemies, enemyImages);
-        drawSidebar(p5);
-        drawHealth(p5, player[0]);
-        drawInventory(p5, player[0].inventory, itemImage);
-        drawBorder(p5);
-        updateGuy(p5, player[0], map, startCorner, frame);
-        updateEnemies(p5, enemies, map, startCorner, frame);
-        updateHealth(player[0], enemies);
-        frame++;
-        frame = frame % 100;
+        if (state === 'PLAY') {
+            next = shiftView(player, p5, startCorner, enemies, mapImage, map);
+            if (startCorner.col === next.col && startCorner.row === next.row) {
+                drawBackground(p5, startCorner, mapImage);
+                drawEntities(p5, map, startCorner, enemies, itemImage);
+                drawGuy(p5, player[0], images);
+                drawEnemies(p5, enemies, enemyImages);
+                drawSidebar(p5);
+                drawHealth(p5, player[0]);
+                drawInventory(p5, player[0].inventory, itemImage);
+                drawBorder(p5);
+                updateGuy(p5, player[0], map, startCorner, frame);
+                updateEnemies(p5, enemies, map, startCorner, frame);
+                updateHealth(player[0], enemies);
+                frame++;
+                frame = frame % 100;
+            } else {
+                superMove = startCorner.row - next.row;
+                state = 'GLIDE';
+            }
+        } else if (state === 'GLIDE') {
+            let move = startCorner.row - next.row;
+
+            //
+            drawBackground(p5, startCorner, mapImage);
+            //
+
+            player[0].ypos += (superMove * BS - 1) / 30;
+            enemies.forEach(e => e.ypos += (superMove * BS - 1) / 30);
+
+            //
+            drawGuy(p5, player[0], images);
+            //
+
+
+            startCorner.row -= (HEIGHT_UNITS - 5) / 30;
+
+            if (move <= 0) {
+                startCorner.row = next.row;
+                player[0].ypos -= (superMove * BS - 1) / 30;
+                enemies.forEach(e => e.ypos -= (superMove * BS - 1) / 30);
+                state = 'PLAY';
+            }
+        }
     }
+}
+const drawBackground = (p5, startCorner, mapImage) => {
+    p5.image(mapImage, 0, 0, p5.width, p5.height, 32 + startCorner.col * BS,
+        32 + startCorner.row * BS, p5.width, p5.height);
 }
 
 const drawSidebar = p5 => {
--- src/shiftView.js
+++ src/shiftView.js
@@ -1,48 +1,56 @@
 import { BS, HEIGHT_UNITS, WIDTH_UNITS } from './constants';
-import { SSL_OP_MICROSOFT_BIG_SSLV3_BUFFER } from 'constants';
 
 export const shiftView = (player, p5, startCorner, enemies, mapImage, map) => {
     // handle the shifting of the map, so we only draw the visible portion, and shift entities accordingly
+    let sC = {};
+    sC.row = startCorner.row;
+    sC.col = startCorner.col;
+
     if (player[0].xpos !== undefined) {
-        if (p5.height - (player[0].ypos + BS) < 2 * BS) {
-            let canMove = map.length - HEIGHT_UNITS - startCorner.row;
-            let wantToMove = HEIGHT_UNITS - 5;
-            let move = Math.min(canMove, wantToMove);
-            startCorner.row += move;
-            player[0].ypos -= move * BS + 1;
-            enemies.forEach(e => e.ypos -= move * BS + 1);
-        }
 
+        // player going down
+        // if (p5.height - (player[0].ypos + BS) < 2 * BS) {
+        //     let canMove = map.length - HEIGHT_UNITS - sC.row;
+        //     let wantToMove = HEIGHT_UNITS - 5;
+        //     let move = Math.min(canMove, wantToMove);
+        //     sC.row += move;
+        //     // player[0].ypos -= move * BS + 1;
+        //     // enemies.forEach(e => e.ypos -= move * BS + 1);
+        // }
+
+        // player going up
         if (player[0].ypos < 2 * BS) {
-            let canMove = startCorner.row;
+            let canMove = sC.row;
             let wantToMove = HEIGHT_UNITS - 5;
             let move = Math.min(canMove, wantToMove);
-            startCorner.row -= move;
-            player[0].ypos += move * BS - 1;
-            enemies.forEach(e => e.ypos += move * BS - 1);
+            sC.row -= move;
+            // player[0].ypos += move * BS - 1;
+            // enemies.forEach(e => e.ypos += move * BS - 1);
         }
 
-        if (p5.width - (player[0].xpos + BS) < 2 * BS + 160) {
-            let canMove = map[0].length - WIDTH_UNITS - startCorner.col + 5;
-            let wantToMove = WIDTH_UNITS - 10;
-            let move = Math.min(canMove, wantToMove);
-            startCorner.col += move;
-            player[0].xpos -= move * BS + 1;
-            enemies.forEach(e => e.xpos -= move * BS - 1);
-        }
+        // // player going right
+        // if (p5.width - (player[0].xpos + BS) < 2 * BS + 160) {
+        //     let canMove = map[0].length - WIDTH_UNITS - sC.col + 5;
+        //     let wantToMove = WIDTH_UNITS - 10;
+        //     let move = Math.min(canMove, wantToMove);
+        //     sC.col += move;
+        //     // player[0].xpos -= move * BS + 1;
+        //     // enemies.forEach(e => e.xpos -= move * BS - 1);
+        // }
 
-        if (player[0].xpos < 2 * BS) {
-            let canMove = startCorner.col;
-            let wantToMove = WIDTH_UNITS - 10;
-            let move = Math.min(canMove, wantToMove);
-            startCorner.col -= move;
-            player[0].xpos += move * BS - 1;
-            enemies.forEach(e => e.xpos += move * BS + 1);
-        }
+        // // player going left
+        // if (player[0].xpos < 2 * BS) {
+        //     let canMove = sC.col;
+        //     let wantToMove = WIDTH_UNITS - 10;
+        //     let move = Math.min(canMove, wantToMove);
+        //     sC.col -= move;
+        //     // player[0].xpos += move * BS - 1;
+        //     // enemies.forEach(e => e.xpos += move * BS + 1);
+        // }
     }
 
-    p5.image(mapImage, 0, 0, p5.width, p5.height, 32 + startCorner.col * BS,
-        32 + startCorner.row * BS, p5.width, p5.height);
+    // p5.image(mapImage, 0, 0, p5.width, p5.height, 32 + sC.col * BS,
+    //     32 + sC.row * BS, p5.width, p5.height);
 
-    return startCorner;
+    return sC;
 }
