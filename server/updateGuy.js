f712c7a4a13bedc678ef6ab72d0eb59156e069e2
Zachary Blasczyk <zbsmith777@gmail.com>
2018-05-18 20:38:19 -0500

    testing movment

--- server/updateGuy.js
+++ server/updateGuy.js
@@ -8,28 +8,28 @@ exports.updateGuy = (id, dir, io) => {
     let { players, map } = getState();
     let p = players[id];
 
-    if (dir === 'up') {
+    if (dir.up) {
         p.direction = 'up';
         if (processGuy(Math.floor((p.ypos - p.speed) / BS), undefined, p, io))
             p.ypos -= p.speed;
         advanceStep(p);
     }
 
-    if (dir === 'down') {
-        p.direction = 'playerdown';
+    if (dir.down) {
+        p.direction = 'down';
         if (processGuy(Math.floor((p.ypos + p.speed + BS - 1) / BS), undefined, p, io))
             p.ypos += p.speed;
         advanceStep(p);
     }
 
-    if (dir === 'left') {
+    if (dir.left) {
         p.direction = 'left';
         if (processGuy(undefined, Math.floor((p.xpos - p.speed) / BS), p, io))
             p.xpos -= p.speed;
         advanceStep(p);
     }
 
-    if (dir === 'right') {
+    if (dir.right) {
         p.direction = 'right';
         if (processGuy(undefined, Math.floor((p.xpos + p.speed + BS - 1) / BS), p, io))
             p.xpos += p.speed;
--- src/updateGuy.js
+++ src/updateGuy.js
@@ -1,12 +1,23 @@
-import { processGuy } from './processGuy';
-import { BS, SPEED } from './constants';
-import { getState, setState } from './globalState';
+import { getState } from './globalState';
 
 export const updateGuy = (p5, socket) => {
     let { id } = getState();
+    let dir = { 'up': false, 'down': false, 'left ': false, 'right': false }
 
-    if (p5.keyIsDown(p5.UP_ARROW)) socket.emit('playermove', { id, dir: 'up' });
-    if (p5.keyIsDown(p5.DOWN_ARROW)) socket.emit('playermove', { id, dir: 'down' });
-    if (p5.keyIsDown(p5.LEFT_ARROW)) socket.emit('playermove', { id, dir: 'left' });
-    if (p5.keyIsDown(p5.RIGHT_ARROW)) socket.emit('playermove', { id, dir: 'right' });
+    if (p5.keyIsDown(p5.UP_ARROW)) dir.up = true;
+    if (p5.keyIsDown(p5.DOWN_ARROW)) dir.down = true;
+    if (p5.keyIsDown(p5.LEFT_ARROW)) dir.left = true;
+    if (p5.keyIsDown(p5.RIGHT_ARROW)) dir.right = true;
+
+    if (dir.left === true && dir.right === true) {
+        dir.left = false;
+        dir.right = false;
+    }
+
+    if (dir.up === true && dir.down === true) {
+        dir.up = false;
+        dir.down = false;
+    }
+
+    socket.emit('playermove', { id, dir });
 }
