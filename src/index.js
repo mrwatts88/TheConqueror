a41da7a32fc1ba93b6d76e0afa837ef2cdfea837
Zachary Blasczyk <zbsmith777@gmail.com>
2018-05-20 14:49:18 -0500

    now ture what is wrong

--- src/index.js
+++ src/index.js
@@ -46,8 +46,8 @@ chatSendBtn.addEventListener('click', e => {
     });
 })
 
-// const socket = io.connect('http://localhost:8080');
-const socket = io.connect('http://184.58.143.70:8080');
+const socket = io.connect('http://localhost:8080');
+// const socket = io.connect('http://184.58.143.70:8080');
 
 // When the client is first served the page, it will connect to the websocket server
 // The server will then send the map to the client
